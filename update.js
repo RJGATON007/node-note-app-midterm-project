d6f9fcae9a29aab82544af91253a997d52c2a25d
RJGATON007 <robertgaton2017@gmail.com>
2022-10-20 15:03:25 +0800

    up

--- update.js
+++ update.js
@@ -3,3 +3,12 @@ const fs = require('fs')
 const update = function(note, oldNote) {
     const newNote = JSON.parse(oldNote).map(function(n,i) {
         if(n.id == note.id) {
+            n.title = note.title
+            n.body = note.body
+        }
+        return n
+    })
+    fs.writeFileSync('note.txt', JSON.stringify(newNote))
+}
+
+module.exports = update
