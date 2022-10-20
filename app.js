a75840d6586430f671aedb93dd3896a09938db7d
RJGATON007 <robertgaton2017@gmail.com>
2022-10-20 13:56:41 +0800

    app.js updated 2

--- app.js
+++ app.js
@@ -24,3 +24,25 @@ if(cmd[2] == 'read') {
     present(read())
 }
 
+if(cmd[2]==="delete"){
+    const id=cmd[3]
+    const oldNote=read()
+    const del=require('./del')
+  
+    del(id,oldNote)
+    console.log(read())
+  }
+  
+  if(cmd[2] == 'update') {
+      const note = {
+          id: cmd[3],
+          title: cmd[4],
+          body: cmd[5] 
+      }
+  
+      const oldNote = read()
+  
+      update(note, oldNote)
+  
+      present(read())
+  }
